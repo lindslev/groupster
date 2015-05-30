6069bf9d8966c0a49c6c66dede736e871023f84a
Lindsay <ilinds4@aol.com>
2015-05-30 16:33:05 -0400

    added preferred servers settings and private group / joiner toggle

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,61 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+chrome.storage.local.get('servers', function(val) {
+		if(!val) {
+			chrome.storage.local.set({'servers': ['Radius', 'Segment', 'Pi', 'Origin', 'Sphere', 'Centra', 'Arc', 'Orbit', 'Chord', 'Diameter', 'Maptest2', 'Tangent']
+		    }, function() {
+			  console.log('Initialized.');
+			});
+		}
+	})
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+chrome.storage.local.get('groups', function(val) {
+		if(!val) {
+			chrome.storage.local.set({'groups': true
+		    }, function() {
+			  console.log('Initialized.');
+			});
+		}
+	})
 
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+	if(message.method == 'preferences') {
+		chrome.storage.local.set({'servers': message.args.servers
+		    }, function() {
+		  console.log('Saved.');
+		});
+	}
+	if(message.method == 'groups') {
+		chrome.storage.local.set({'groups': message.args.groups
+		    }, function() {
+		  console.log('Saved.');
+		});
+	}
+});
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+function getServers() {
+	chrome.storage.local.get('servers', function(val) {
+		chrome.extension.sendRequest({method: val}, function(response) {
+        	
+      	});
+	})
+}
+
+function joinerOrGroup() {
+	chrome.storage.local.get('groups', function(val) {
+		chrome.extension.sendRequest({method: val}, function(response) {
+        	
+      	});
+	})
+}
+
+/*** just for dev purposes really ***/
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
+});
